6b7147f1b50315596deda0d3fd3fff4c3e2a9b34
AmaniAdam0607 <amaniadam178@gmail.com>
2024-03-19 16:53:01 -0700

    added some resposiveness and added whitenoise

--- cms/settings.py
+++ cms/settings.py
@@ -12,9 +12,12 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 from django.urls import reverse_lazy
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+STATIC_ROOT = BASE_DIR / "static"
 
 
 # Quick-start development settings - unsuitable for production
@@ -55,6 +58,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -67,6 +71,11 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'cms.urls'
+
+
+ALLOWED_HOSTS = ['AmaniAdam.pythonanywhere.com']
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 TEMPLATES = [
     {
@@ -83,6 +92,15 @@ TEMPLATES = [
         },
     },
 ]
+
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 WSGI_APPLICATION = 'cms.wsgi.application'
 
